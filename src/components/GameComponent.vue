1ea2146b936207ea60b38040aeb71ffcb3da5826
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-12-22 11:01:23 +0100

    minor updates

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -1,34 +1,61 @@
 <template>
     <h2>Game Component</h2>
     <div ref="gameboard">
-        <p>
-            This is a message
-        </p>
+        <table>
+            <tr v-for="row in rows">
+                <td v-for="col in columns" class="border square">
+                    {{ items.pop() }}
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
-<style></style>
+<style>
+.border {
+    border: 1px;
+    border-color: black;
+    border-style: solid;
+}
+
+.square {
+    width: 4em;
+    height: 4em;
+}
+</style>
 
 <script lang="ts">
 export default {
     data() {
         return {
-            items: [] // 2D Array
+            items: [], // 2D Array
+            rows: 0,
+            columns: 0,
         }
     },
     methods: {
-        createBoard() {
+        createBoard(this: any) {
             // Debugging
             console.log('Setup a Board');
-            let width = 10, height = 8
+            let width = 5, height = 5
 
-            // Get the Board
-            let board = this.$refs.gameboard as HTMLDivElement;
+            // Copy the Board State into Items
+            this.items = [
+                ['a', 'b', 'c', 'd', 'e'],
+                ['a', 'b', 'c', 'd', 'e'],
+                ['a', 'b', 'c', 'd', 'e'],
+                ['a', 'b', 'c', 'd', 'e'],
+                ['a', 'b', 'c', 'd', 'e']
+            ]
+            
+            // Calculate these using the Board.ts
+            this.rows = height;
+            this.columns = width;
 
-            // testing of stuff
-            this.items = [['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e']]
+            // Flatten the Items
+            this.items = this.items.flatMap((v: any) => v)
 
-            // Set the Board
+            // Debugging
             console.log('Created Empty Board')
         },
     },
